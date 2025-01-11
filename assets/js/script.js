19f11abc92aff612dd88c0243f894d498bb2fcf6
DSnake0 <revilwhoami0098@gmail.com>
2025-01-11 20:44:08 +0500

    Add new Swipper

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
-const swiper = new Swiper('.swiper', {
-    direction: 'vertical',
-    loop: true,
-});
+function firstSwipper() {
+    const swiper = new Swiper('.swiper', {
+        direction: 'vertical',
+        loop: true,
+    });
+};
 
+function secondSwipper() {
+    const customSwiper = new Swiper('.customSwiper', {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        loop: true,
+    });
+};
+
+
+firstSwipper();
+secondSwipper();
