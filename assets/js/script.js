ab3e5b7a916e2f8d3ba15dbdf2f4e271f2d0b8cd
DSnake0 <revilwhoami0098@gmail.com>
2025-01-13 15:24:38 +0500

    minor changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,6 @@ function secondSwiper() {
     const swiper = new Swiper(".mySwiper2", {
         slidesPerView: 3,
         spaceBetween: 30,
-        pagination: {
-            el: ".swiper-pagination2",
-            clickable: true,
-        },
     });
 };
 
