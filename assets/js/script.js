ee8e7ed7d30ee901197d9899bcc8394cf01d4ecc
DSnake0 <revilwhoami0098@gmail.com>
2025-01-14 20:21:10 +0500

    Add main Section 03 and also add main sectoin 04 and also add footer with responsivness

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,5 +14,26 @@ function swiper1() {
     });
 }
 
+function swiper2() {
+    const swiper2 = new Swiper(".mySwiper2", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        breakpoints: {
+            // When the viewport width is >= 320px
+            320: {
+                slidesPerView: 1
+            },
+            // When the viewport width is >= 768px
+            768: {
+                slidesPerView: 2
+            },
+            // When the viewport width is >= 1024px
+            1024: {
+                slidesPerView: 3
+            },
+        },
+    });
+}
 
 swiper1();
+swiper2();
