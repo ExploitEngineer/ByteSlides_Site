773d239a758a92eaae22d07bae26487ab202278e
DSnake0 <revilwhoami0098@gmail.com>
2025-01-14 12:48:44 +0500

    Add Main Section 02 & also Add new images from unsplash

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function swiper1() {
+    const swiper = new Swiper(".mySwiper1", {
+        grabCursor: true,
+        effect: "creative",
+        creativeEffect: {
+            prev: {
+                shadow: true,
+                translate: [0, 0, -400],
+            },
+            next: {
+                translate: ["100%", 0, 0],
+            },
+        },
+    });
+}
+
+
+swiper1();
